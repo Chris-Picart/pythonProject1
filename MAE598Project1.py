3006003cdeb4192e1a2c2e1b1788c5c4ab641223
Chris-Picart <112508368+Chris-Picart@users.noreply.github.com>
2022-10-27 20:51:48 -0700

    Update MAE598Project1.py

--- MAE598Project1.py
+++ MAE598Project1.py
@@ -8,22 +8,18 @@ import time
 import torch as t
 import torch.nn as nn
 from torch import optim
+import torch
 from torch.nn import utils
 import matplotlib.pyplot as plt
 
 logger = logging.getLogger(__name__)
 # environment parameters
+# values below are normalized to prevent loss from "exploding" this way reactions are minimized to keep
+FRAME_TIME = .5  # time interval
+GRAVITY_ACCEL = 9.8/1000  # gravity constant
+ROTATION_ACCEL = 9/1000  # rotation constant
+BOOST_ACCEL = 12./1000
 
-FRAME_TIME = .1  # time interval
-GRAVITY_ACCEL = 9.8  # gravity constant
-ROTATION_ACCEL = 20  # rotation constant
-BOOST_ACCEL = 12
-
-# # the following parameters are not being used in the sample code
-# PLATFORM_WIDTH = 0.25  # landing platform width
-# PLATFORM_HEIGHT = 0.06  # landing platform height
-# ROTATION_ACCEL = 20  # rotation constant
-# define system dynamics
 # Notes:
 # 0. You only need to modify the "forward" function
 # 1. All variables in "forward" need to be PyTorch tensors.
@@ -46,21 +42,35 @@ class Dynamics(nn.Module):
         state[4] = theta
         """
 
-        # Create Dynamic matricies
-        # Note: Here gravity is used to change velocity which is the second element of the state vector
-        # Normally, we would do x[1] = x[1] + gravity * delta_time
-        # but this is not allowed in PyTorch since it overwrites one variable (x[1]) that is part of the computational graph to be differentiated.
-        # Therefore, I define a tensor dx = [0., gravity * delta_time], and do x = x + dx. This is allowed...
-        delta_state_gravity = t.tensor([0, 0.5* GRAVITY_ACCEL * FRAME_TIME, 0., 0., 0.])
+        # Here the dynamic equations are split into matricies of tensor form so that values can be updated without overwriting
+        # a computational position with a value. This allows the tensor to be continuously differentiated at each step.
+
+        delta_state_gravity = t.tensor([0, 0.5 * GRAVITY_ACCEL * FRAME_TIME, 0., 0., 0.]) # Setting up influence of gravity
+        # on y velocity. Only in the position corresponding to velocity in y direction because it has no influence on the other
+        # state variables
+
+        #Drag force- F = 0.5 * rho_air * v^2 * Cd * Cross-sectional Area
+        # Cross sectional area taken to = 1, density of air = 1.225kg/m^3
 
         # Thrust
         # Note: Same reason as above. Need a 2-by-1 tensor.
-        delta_dot_state =  t.tensor([[0., 0.], [math.cos(state[4]) * FRAME_TIME, 0.], [0., 0.], [math.sin(state[4]) * FRAME_TIME, 0.], [0., 1.]])
-        delta2_state = t.tensor([-1 * action[0] * BOOST_ACCEL, -1 * action[1] * ROTATION_ACCEL])
-        delta_state = t.matmul(delta_dot_state, delta2_state) # issue is probably arising from indexin state[4] here.
+        # delta_dot_state = t.tensor([[0., 0.], [torch.cos(state[4]) * FRAME_TIME, 0.], [0., 0.], [-torch.sin(state[4]) * FRAME_TIME, 0.], [0., 1.]])
+        # delta2_state = t.tensor([-1 * action[0] * BOOST_ACCEL, -1 * action[1] * ROTATION_ACCEL])
+
+       #Need to use torch. operations on the tensors
+
+        delta_dot_state = t.zeros((5, 2))
+        delta_dot_state[1, 0] = torch.cos(state[4]) * FRAME_TIME
+        delta_dot_state[3, 0] = -torch.sin(state[4]) * FRAME_TIME
+        delta_dot_state[4, 1] = 1
+
+        delta2_state = t.zeros(2)
+        delta2_state[0] = 1 * action[0] * BOOST_ACCEL
+        delta2_state[1] = -1 * action[1] * ROTATION_ACCEL
+        delta_state = t.matmul(delta_dot_state, delta2_state)  # issue is probably arising from indexin state[4] here.
 
         # Update velocity
-        state = state + delta_state + delta_state_gravity
+        state = state + delta_state - delta_state_gravity
 
         # Update state
         # Note: Same as above. Use operators on matrices/tensors as much as possible. Do not use element-wise operators as they are considered inplace.
@@ -72,32 +82,6 @@ class Dynamics(nn.Module):
         state = t.matmul(step_mat, state)
 
         return state
-
-
-# Demonstrate the inplace operation issue
-
-# class Dynamics(nn.Module):
-#
-#     def __init__(self):
-#         super(Dynamics, self).__init__()
-#
-#     @staticmethod
-#     def forward(state, action):
-#         """
-#         action: thrust or no thrust
-#         state[0] = y
-#         state[1] = y_dot
-#         """
-#
-#         # Update velocity using element-wise operation. This leads to an error from PyTorch.
-#         state[1] = state[1] + GRAVITY_ACCEL * FRAME_TIME - BOOST_ACCEL * FRAME_TIME * action
-#
-#         # Update state
-#         step_mat = t.tensor([[1., FRAME_TIME],
-#                              [0., 1.]])
-#         state = t.matmul(step_mat, state)
-#
-#         return state
 
 
 # a deterministic controller
@@ -118,10 +102,11 @@ class Controller(nn.Module):
         super(Controller, self).__init__()
         self.network = nn.Sequential(
             nn.Linear(dim_input, dim_hidden),
+            nn.Sigmoid(),
+            nn.Linear(dim_hidden, dim_output),
             nn.Tanh(),
-            nn.Linear(dim_hidden, dim_hidden),
+            nn.Linear(dim_output, dim_hidden),
             nn.Tanh(),
-            nn.Linear(dim_hidden, dim_output)
         )
 
     def forward(self, state):
@@ -157,11 +142,11 @@ class Simulation(nn.Module):
 
     @staticmethod
     def initialize_state():
-        state = [10, 0., 1, 0., .2617]  # TODO: need batch of initial states
-        return t.tensor(state, requires_grad=True).float()
+        state = [2, 0., 1, 0., .24]  # TODO: need batch of initial states
+        return t.tensor(state, requires_grad=False).float()
 
     def error(self, state):
-        return state[0] ** 2 + state[1] ** 2 + state[2] ** 2 + state[3] ** 2 + math.sin(state[4])
+        return torch.mean(state ** 2) # Using mean squared error to get loss values faster
 
 
 # set up the optimizer
@@ -176,7 +161,10 @@ class Optimize:
     def __init__(self, simulation):
         self.simulation = simulation
         self.parameters = simulation.controller.parameters()
-        self.optimizer = optim.LBFGS(self.parameters, lr=0.01)
+        self.optimizer = optim.SGD(self.parameters, lr=0.01)
+        #lr = .05 is too large, causes "overstepping"
+        #best lr value found from experimentation is .01
+
 
     def step(self):
         def closure():
@@ -192,32 +180,35 @@ class Optimize:
         for epoch in range(epochs):
             loss = self.step()
             print('[%d] loss: %.3f' % (epoch + 1, loss))
-            self.visualize()
+            # self.visualize()
 
-    def visualize(self):
-        data = np.array([self.simulation.state_trajectory[i].detach().numpy() for i in range(self.simulation.T)])
-        y = data[:, 0]
-        y_dot = data[:, 1]
-        x = data[:, 2]
-        x_dot = data[:, 3]
-        theta = data[:, 4]
-        plt.plot(y, label="Height", linestyle="-")
-        plt.plot(y_dot, label="Vertical Velocity", linestyle="--")
-        plt.plot(x, label="Horizontal location", linestyle="-.")
-        plt.plot(x_dot,  label="Horizontal velocity", linestyle=":")
-        plt.plot(theta, label="Orientation", linestyle=":")
-        plt.legend()
-        plt.show()
+    # def visualize(self):
+    #     data = np.array([self.simulation.state_trajectory[i].detach().numpy() for i in range(self.simulation.T)])
+    #     y = data[:, 0]
+    #     y_dot = data[:, 1]
+    #     x = data[:, 2]
+    #     x_dot = data[:, 3]
+    #     theta = data[:, 4]
+    #     plt.plot(y, label="Height", linestyle="-")
+    #     plt.plot(y_dot, label="Vertical Velocity", linestyle="--")
+    #     plt.plot(x, label="Horizontal location", linestyle="-.")
+    #     plt.plot(x_dot,  label="Horizontal velocity", linestyle=":")
+    #     plt.plot(theta, label="Orientation", linestyle=":")
+    #     plt.legend()
+    #     plt.show()
 
 
 # Now it's time to run the code!
 
-T = 100 # number of time steps
+T = 100 # number of time steps increasing T and reducing time frame leads to faster convergence but takes longer to iterate
 dim_input = 5  # state space dimensions
-dim_hidden = 6  # latent dimensions
+dim_hidden = 10  # latent dimensions max convergence at i = 27
 dim_output = 2  # action space dimensions ;
 d = Dynamics()  # define dynamics
 c = Controller(dim_input, dim_hidden, dim_output)  # define controller
 s = Simulation(c, d, T)  # define simulation
 o = Optimize(s)  # define optimizer
-o.train(10)  # solve the optimization problem
+o.train(50)  # solve the optimization problem
+
+# notes: Need to use small learning rate, .005 had best convergence. Since learning rate is small, set of time (time interval * T )
+#needs to be small or the initial loss is too large to converge in 50 iterations.
